5a9210e65ed1bd621eb02b3a1e9d80f513a4f9b1
Furqan Mehmood <furqanizgenius@gmail.com>
2025-12-30 18:20:24 +0500

    first commit

---
+++ backend/App.js
@@ -0,0 +1,25 @@
+import express, { urlencoded } from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import { dbConnection } from "./database/dbConnection.js"
+import reservationRouter from "./routes/reservationRoute.js"
+import { errorMiddleware } from "./error/error.js"
+
+const app = express()
+dotenv.config({ path: './config/config.env' })
+
+app.use(cors({
+    // origin: [process.env.FRONTEND_URL],
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/api/v1/reservation', reservationRouter)
+
+dbConnection()
+
+app.use(errorMiddleware)
+export default app;
